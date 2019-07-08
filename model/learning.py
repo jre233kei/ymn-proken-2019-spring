54f87a5f4e785cc6122705266c21042feabb9dfb
jre233kei <jre233kei+github@gmail.com>
2019-07-08 10:59:28 +0900

    [Fix] Credential

--- model/learning.py
+++ model/learning.py
@@ -5,4 +5,5 @@ argvs = sys.argv
 input_file = argvs[1]
 output = argvs[2]
 
-classifier = ft.supervised(input_file, output)
+classifier = ft.train_supervised(input_file)
+classifier.save_model(output)
