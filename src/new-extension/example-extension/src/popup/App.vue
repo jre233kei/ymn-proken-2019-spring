f5da0f463ae37dfc6f8ad61bfedb08bd317cc194
jre233kei <jre233kei+github@gmail.com>
2019-07-22 13:24:53 +0900

    [Add] Vue files

---
+++ src/new-extension/example-extension/src/popup/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <p>Hello world!</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+//axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
+export default {
+  data () {
+    return {}
+  },
+  mounted () {
+    axios.post('https://g8kwped00b.execute-api.us-east-1.amazonaws.com/production', {
+      headers : {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+        //"Access-Control-Allow-Credentials": "true"
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+      },
+      body: {
+        "text": '最悪の製品でした。'
+      }
+    }).then(response => {
+      alert('結果: ' + response.data.text);
+    }).catch(error => {
+      alert(error);
+    });;
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
